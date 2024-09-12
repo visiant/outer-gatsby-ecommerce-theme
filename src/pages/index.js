921d695fdfd1ffb1c50cf9624d41102f9bea8231
visiant <47551136+visiant@users.noreply.github.com>
2024-09-12 12:21:40 -0600

    Hero change

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ const IndexPage = () => {
       {/* Hero Container */}
       <Hero
         maxWidth={'500px'}
-        image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        image={'/outer_canyon_design.jpg'}
+        title={'Wear to explore your wild heart'}
+        subtitle={'Discover Spring Summer 2025'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
